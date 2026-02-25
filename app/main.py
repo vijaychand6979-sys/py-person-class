b4e9f66ed93090b1a38caa64bad84631cd6df1d7
Vijay Chand <vijaychand6979@googlemail.com>
2026-02-25 09:46:59 +0000

    refactored code

--- app/main.py
+++ app/main.py
@@ -11,16 +11,14 @@ def create_person_list(people: list) -> list:
     persons = [Person(person["name"], person["age"]) for person in people]
 
     for person in people:
-        if person.get("wife") and person.get("wife") is not None:
-            spouse = person["wife"]
-            husband = person["name"]
-            wife = Person.people[spouse]
-            Person.people[husband].wife = wife
+        wife = person.get("wife")
+        husband = person.get("husband")
+        person_obj = Person.people.get(person.get("name"))
 
-        if person.get("husband") and person.get("husband") is not None:
-            spouse = person["husband"]
-            wife = person["name"]
-            husband = Person.people[spouse]
-            Person.people[wife].husband = husband
+        if wife and wife is not None:
+            person_obj.wife = Person.people.get(wife)
+
+        if husband and husband is not None:
+            person_obj.husband = Person.people.get(husband)
 
     return persons
